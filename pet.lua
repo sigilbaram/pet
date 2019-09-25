356c2c642050569e41bfad1a9adc43bc6fcfc3d1
Sigil Baram <sigilbaram@gmail.com>
2019-09-25 02:37:22 -0400

    Item id offsets

--- pet.lua
+++ pet.lua
@@ -1,4 +1,44 @@
+local bit = require('bit')
+
+local items = require('client_data.items')
 local client = require('shared.client')
 
 local data, ftype = client.new('pet_service')
+
+ftype.fields.automaton.type.fields.attachments.type.base.fields.item = {
+    get = function(data)
+        return items[data.item_id]
+    end,
+}
+
+ftype.fields.automaton.type.fields.heads_available = {
+    get = function(data)
+        return setmetatable({}, {
+            __index = function(t, k)
+                return data.available_heads[k - 0x2000]
+            end,
+        })
+    end,
+}
+
+ftype.fields.automaton.type.fields.frames_available = {
+    get = function(data)
+        return setmetatable({}, {
+            __index = function(t, k)
+                return data.available_frames[k - 0x2020]
+            end,
+        })
+    end,
+}
+
+ftype.fields.automaton.type.fields.attach_available = {
+    get = function(data)
+        return setmetatable({}, {
+            __index = function(t, k)
+                return data.available_attach[k - 0x2100]
+            end,
+        })
+    end,
+}
+
 return data
